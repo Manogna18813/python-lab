5dd387f76e26624684ee0754d7070f2ba3edccfd
Manogna18813 <majjimanogna@gmail.com>
2025-12-14 17:53:53 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+print("value of pi: ",math.pi)
+num=int(input("Enter a number to find the square: "))
+print("square root of the number ",num,"is: ",math.sqrt(num))
+angle_deg=30
+angle_rad=math.radians(angle_deg)
+print("sin(",angle_deg,"degrees)= ",math.sin(angle_rad))
+angle_deg=30
+angle_rad=math.radians(angle_deg)
+print("cos(",angle_deg,"degrees)= ",math.cos(angle_rad))
