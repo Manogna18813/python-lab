2a421061e1d0f1d7e1fcb49efa60683ce6bed15a
Manogna18813 <majjimanogna@gmail.com>
2025-12-17 18:28:07 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,8 @@
+import numpy as nps
+# 1D Array
+arr1 = np.array([10, 20, 30, 40, 50])
+print("Median of 1D array:", np.median(arr1))  
+# 2D Array
+arr2 = np.array([[1, 3, 5],
+                 [2, 4, 6]])
+print("Median of 2D array (overall):", np.median(arr2)) 
