53d5d395671fd584b7b49286053affb05f9a8514
Manogna18813 <majjimanogna@gmail.com>
2026-03-03 18:29:58 +0530

    Create dictionary methods.py

---
+++ dictionary methods.py
@@ -0,0 +1,9 @@
+student = {"name": "manu", "age": 20, "grade": "A"}
+print("Keys:", student.keys())
+print("Values:", student.values())
+print("Items:", student.items())
+removed_value = student.pop("age")
+print("Removed Value (age):", removed_value)
+print("Dictionary after pop():", student)
+del student["grade"]
+print("Dictionary after delete:", student)
